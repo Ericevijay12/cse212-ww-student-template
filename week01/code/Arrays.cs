52b58e2fba4d8142c32e8cdbd8e383e6bfe67633
Ericevijay12 <ericevijayohiani@gmail.com>
2026-03-11 06:01:37 +0100

    Refactor MultiplesOf function and improve RotateListRight

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,33 +1,38 @@
+using System;
+using System.Collections.Generic;
+
 public static class Arrays
 {
-    /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
-    /// integer greater than 0.
-    /// </summary>
-    /// <returns>array of doubles that are the multiples of the supplied number</returns>
-    public static double[] MultiplesOf(double number, int length)
+    public static double[] MultiplesOf(double number, int count)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Plan:
+        // 1. Create a new array of doubles called 'results' with size 'count'.
+        // 2. Loop through the array using a for-loop from index 0 to count-1.
+        // 3. For each index, calculate the multiple as: number * (index + 1).
+        // 4. Return the 'results' array.
 
-        return []; // replace this return statement with your own
+        double[] results = new double[count];
+        for (int i = 0; i < count; i++)
+        {
+            results[i] = number * (i + 1);
+        }
+        return results;
     }
 
-    /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
-    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
-    ///
-    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
-    /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Plan:
+        // 1. Return early if the list is empty to avoid errors.
+        // 2. Use modulo (%) to ensure 'amount' is within the list bounds.
+        // 3. Find the split point: data.Count - effective amount.
+        // 4. Use GetRange to copy the end part, then RemoveRange and InsertRange.
+
+        if (data.Count == 0) return;
+        int effectiveAmount = amount % data.Count;
+        if (effectiveAmount == 0) return;
+
+        List<int> suffix = data.GetRange(data.Count - effectiveAmount, effectiveAmount);
+        data.RemoveRange(data.Count - effectiveAmount, effectiveAmount);
+        data.InsertRange(0, suffix);
     }
 }
